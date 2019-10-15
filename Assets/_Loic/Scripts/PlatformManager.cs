45c5f3fd7119ad301810e99ec485b1dfa5e4f04e
Aiola <barth.13arles@hotmail.fr>
2019-10-15 11:38:37 +0200

    Add SnapZoneDrop HeadSet

--- Assets/_Loic/Scripts/PlatformManager.cs
+++ Assets/_Loic/Scripts/PlatformManager.cs
@@ -5,13 +5,16 @@ using VRTK;
 
 public class PlatformManager : MonoBehaviour
 {
+    public GameObject Base;
+    public Transform prefab;
+
     // Start is called before the first frame update
     void Start()
     {
         if(gameObject.tag == "Platform")
         {
             Debug.Log("Bonjour");
-            gameObject.GetComponent<Animator>().SetBool("Rise", true);
+            gameObject.GetComponent<Animator>().SetBool("Open", true);
 
         }
     }
@@ -20,5 +23,11 @@ public class PlatformManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OpenBase()
+    {
+        Instantiate(prefab, new Vector3(0.172f, 0, -2.03f), Quaternion.identity);
+        Base.GetComponent<Animator>().SetBool("In", true);
     }
 }
