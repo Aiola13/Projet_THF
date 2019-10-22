9436c30018b20f14c47ad9c76848781de5c70082
Aiola <barth.13arles@hotmail.fr>
2019-10-22 15:09:02 +0200

    Add Caisse RFIG + Add Shader Wireframe

---
+++ Assets/_Loic/Scripts/GetLocalPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetLocalPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GetLocalPosition");
+        var render = GetComponentsInChildren<Renderer>();
+        Debug.Log(render.Length);
+        
+        for(int i = 0; i > render.Length; i++)
+        {
+            Debug.Log(render[i].bounds.center);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
